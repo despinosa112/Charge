bb2405e7b852f81b5b54c110c0aab60201d21f12
Espinosa, Daniel <DEspinosa@contractor.lb.com>
2020-03-09 19:22:03 -0400

    Initial Commit

---
+++ Charge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Charge
+//
+//  Created by Espinosa, Daniel on 3/9/20.
+//  Copyright © 2020 Espinosa, Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
